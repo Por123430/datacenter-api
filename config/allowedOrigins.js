e62879d68b07c5725ca375eac8036134b4d66c25
Por123430 <por123430@gmail.com>
2023-04-24 22:40:03 +0700

    ready

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000'
+    'https://datacentersecurity.onrender.com/'
     // 'https://www.dandrepairshop.com',
     // 'https://dandrepairshop.com'
 ]
