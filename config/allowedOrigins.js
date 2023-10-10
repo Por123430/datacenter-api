680d9e9f46892d27c107eab8957f983abc0ad780
Por123430 <74410037+Por123430@users.noreply.github.com>
2023-10-10 23:42:31 +0700

    df

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,8 @@
-const allowedOrigins = {
-    
+const allowedOrigins = [
+    'https://datacentersecurity.onrender.com'
     // 'https://www.dandrepairshop.com',
     // 'https://dandrepairshop.com'
-    origin:'https://datacentersecurity.onrender.com',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}
+    
+]
 
 module.exports = allowedOrigins
