b67f991968ce3cbdaba789388426dd39b3b7380d
Por123430 <74410037+Por123430@users.noreply.github.com>
2023-10-10 23:34:19 +0700

    update cor

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,9 @@
-const allowedOrigins = [
-    'https://datacentersecurity.onrender.com'
+const allowedOrigins = {
+    
     // 'https://www.dandrepairshop.com',
     // 'https://dandrepairshop.com'
-]
+    origin:'https://datacentersecurity.onrender.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}
 
 module.exports = allowedOrigins
